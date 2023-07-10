3fb1ad4d148f37fa70e4d3200a41f7becb8cbcbe
Vinicius <viniciushdalaqua@gmail.com>
2023-07-10 10:59:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  //pegar a tag image
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/perfil-light.png")
+  } else {
+    img.setAttribute("src", "./assets/pf.png")
+  }
+}
